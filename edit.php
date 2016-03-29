4974645c3f5899c23418274c3db016eb7b00b496
Tair111 <obra-aleksandr@yandex.ru>
2016-03-29 13:13:34 +0300

    Применил ООП

--- edit.php
+++ edit.php
@@ -1,22 +1,17 @@
 <?php
 
-include_once __DIR__ . ('/models/news.php');
+include_once __DIR__ . ('/models/New.php');
 
 // Приведение переданного через адресную строку параметра id
 // к положительному целому числу.
-if(isset($_GET['id'])){
+
+$model = new News();
+
+if(isset($_GET['id']))
     $id = $_GET['id'];
-}
-
-//
-//
-if(!isset($_POST['ok'])){
-    $article = News_OneArticle($id);
-}
-
 
 // Проверка заполнены ли поля title и content при нажатии "Сохранить".
-if(isset($_POST['ok']) and ($_POST['title'] == "" or $_POST['text'] == "")){
+if(isset($_POST['ok']) and ($_POST['title'] == "" or isset($_POST['ok']) and $_POST['text'] == "")){
     $new_error = true;
 }
 else{
@@ -24,7 +19,7 @@ else{
         $id = $_POST['id'];
         $title = $_POST['title'];
         $text = $_POST['text'];
-        $upd = New_editArticle($id, $title, $text);
+        $upd = $model->New_editArticle($id, $title, $text);
         if($upd)
             $msgOk = "Изменения сохранены.";
         else
@@ -32,7 +27,7 @@ else{
     }
 }
 // Извлечение статьи.
-$article = News_OneArticle($id);
+$article = $model->News_OneArticle($id);
 
 // Кодировка.
 header('Content-type: text/html; charset=utf-8');
