aa7abf8b4b4d9d21cf1555974a3ee1d19dc08c73
Tair111 <obra-aleksandr@yandex.ru>
2016-03-30 16:06:32 +0300

    Добавил классы Storage, View

--- edit.php
+++ edit.php
@@ -1,10 +1,12 @@
 <?php
 
 include_once __DIR__ . ('/models/New.php');
+include_once __DIR__ . ('/Classes/View.php');
 
 // Приведение переданного через адресную строку параметра id
 // к положительному целому числу.
 
+$view = new View();
 $model = new News();
 
 if(isset($_GET['id']))
@@ -12,25 +14,24 @@ if(isset($_GET['id']))
 
 // Проверка заполнены ли поля title и content при нажатии "Сохранить".
 if(isset($_POST['ok']) and ($_POST['title'] == "" or isset($_POST['ok']) and $_POST['text'] == "")){
-    $new_error = true;
+    $view->new_error = true;
 }
 else{
     if(isset($_POST['ok']) and isset($_POST['title']) and isset($_POST['text'])){
         $id = $_POST['id'];
+
         $title = $_POST['title'];
         $text = $_POST['text'];
         $upd = $model->New_editArticle($id, $title, $text);
         if($upd)
-            $msgOk = "Изменения сохранены.";
+            $view->msgOk = "Изменения сохранены.";
         else
-            $msgOk = "Что то пошло не так!";
+            $view->msgOk = "Что то пошло не так!";
     }
 }
 // Извлечение статьи.
-$article = $model->News_OneArticle($id);
-
-// Кодировка.
-header('Content-type: text/html; charset=utf-8');
+$view->article = $model->News_OneArticle($id);
 
 // Вывод в шаблон.
-include('view/edit.php');
+$html = $view->display('edit.php');
+echo $html;
